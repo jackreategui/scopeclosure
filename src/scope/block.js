de70de550339630958297526003949aa1ff6cc41
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-04-15 19:35:43 -0500

    termine la clase de block scope

---
+++ src/scope/block.js
@@ -0,0 +1,13 @@
+function frutas() {
+    if (true) {
+        var fruta1 = 'apple';
+        let fruta2 = 'kiwi';
+        const fruta3 = 'banana';
+        
+        console.log(fruta2);
+        console.log(fruta3);
+    }
+    console.log(fruta1);
+}
+
+frutas()
