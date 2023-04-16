e8b81a4445c569653c5b38459c6ad2d633155d57
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-04-15 19:50:37 -0500

    termine la clase de las declaraciones de las variables

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var primerNombre; //Undefined
+primerNombre = 'jack';
+console.log(primerNombre);
+
+var apellido = 'jackson'; //declarar / asignar
+apellido = 'reategui'; //reasignar
+console.log(apellido);
+
+var segundoNombre = 'fredi'; //declarando / asignando
+var segundoNombre = 'jackson'; //reasignando
+console.log(segundoNombre);
+
+// let
+
+let fruta = 'manzana';
+fruta = 'platano';
+console.log(fruta);
+
+// let fruta = 'kiwi';
+console.log(fruta);
+
+// const
+
+const nombre = 'jack';
+// nombre = 'yeli';
+console.log(nombre);
+
+// const nombre = 'gato';
+
+const carros = [];
+carros.push('toyota');
+console.log(carros);
